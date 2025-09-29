36754d6e3dab4c9f09d60d2a9a0b3cdddfbf5040
Chebart <chebart03@gmail.com>
2025-09-29 18:12:39 +0300

    small changes in sgd

--- core/optimizers/sgd.py
+++ core/optimizers/sgd.py
@@ -4,23 +4,31 @@ class SGD():
     """Stochastic gradient descent optimizer"""
     def __init__(
         self,
-        model_params: list[tuple[np.ndarray, np.ndarray]],
         lr: float = 1e-3,
         momentum: float = 0.0,
         weight_decay: float = 0.0,
         nesterov: bool = False
     ):
-        self.model_params = model_params
-        self.lr = lr
-        self.momentum = momentum
-        self.weight_decay = weight_decay
-        self.nesterov = nesterov
-        self.velocity: float = 0.0
+        self._lr = lr
+        self._momentum = momentum
+        self._weight_decay = weight_decay
+        self._nesterov = nesterov
+        self._velocity: float = 0.0
         
-    def step(self):
-        for p, g in self.model_params:
-            p -= self.lr * g
+    def step(
+        self, 
+        model_params: list[tuple[np.ndarray, np.ndarray]]
+    ):
+        """Update model params"""
+        for p, g in model_params:
+            if self._weight_decay > 0:
+                g += self._weight_decay * g
+            p -= self._lr * g
 
-    def zero_grad(self):
-        for _, g in self.model_params:
+    def zero_grad(
+        self, 
+        model_params: list[tuple[np.ndarray, np.ndarray]]
+    ):
+        """Reset grad of model params"""
+        for _, g in model_params:
             g.fill(0)
