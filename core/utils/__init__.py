b99efeaa744065c53335e7cdb5c5ed19c4db52ea
Artyom Chebykin <chebart03@gmail.com>
2025-10-12 14:34:24 +0300

    added window methods for 2d data

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,2 +1,3 @@
 from .split_data import train_test_split, batch_split
 from .draw_plot import plot_curves
+from .windows import split_2d_data_on_windows
---
+++ core/utils/windows.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def pad_2d_data(
+    x: np.ndarray,
+    pad: int
+)-> np.ndarray:
+    """Adds zero pad to 2d input that pooling or convolution operations can handle borders properly"""
+    return np.pad(
+        x, 
+        ((0, 0), (0, 0), (pad, pad), (pad, pad)), 
+        mode='constant'
+    )
+    
+def split_2d_data_on_windows(
+    x: np.ndarray, 
+    k_size: int, 
+    stride: int,
+    pad: int
+)-> tuple[np.ndarray, np.ndarray]:
+    """Splits a 4D input (N, C, H, W) into smaller 2D sliding windows of size (k_size, k_size) 
+       according to the specified stride and padding"""
+    # Apply padding
+    if pad > 0:
+        x = pad_2d_data(x, pad)
+    N, C, H_in, W_in = x.shape
+    # Output dimensions
+    H_out = (H_in - k_size) // stride + 1
+    W_out = (W_in - k_size) // stride + 1
+
+    # Create sliding windows
+    shape = (N, C, H_out, W_out, k_size, k_size)
+    strides = (
+        x.strides[0],
+        x.strides[1],
+        x.strides[2] * stride,
+        x.strides[3] * stride,
+        x.strides[2],
+        x.strides[3],
+    )
+    windows = np.lib.stride_tricks.as_strided(x, shape=shape, strides=strides)
+
+    return windows, x.shape
