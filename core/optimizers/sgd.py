a16059cdeca8f854ba0fa4b86497dbfc0ee842ee
Artyom Chebykin <chebart03@gmail.com>
2025-11-07 12:08:33 +0300

    updated SGD

--- core/optimizers/sgd.py
+++ core/optimizers/sgd.py
@@ -1,34 +1,40 @@
-import numpy as np
+from typing import Literal, Optional
 
-class SGD():
+from .abstract_optimizer import AbstractOptimizer 
+from ..models import AbstractModel
+
+class SGD(AbstractOptimizer):
     """Stochastic gradient descent optimizer"""
     def __init__(
         self,
-        lr: float = 1e-3,
+        model: AbstractModel,
+        lr: float = 1e-3, 
+        lmbda: float = 0.5,
+        alpha: float = 0.5,
+        reg_type: Optional[Literal["l1", "l2", "elasticnet"]] = None,
         momentum: float = 0.0,
-        weight_decay: float = 0.0,
-        nesterov: bool = False
+        nesterov: bool = False,
     ):
-        self._lr = lr
-        self._momentum = momentum
-        self._weight_decay = weight_decay
-        self._nesterov = nesterov
-        self._velocity: float = 0.0
+        super().__init__(model, lr, lmbda, alpha, reg_type)
+        self.momentum = momentum
+        self.nesterov = nesterov
+        self.velocity: list = [0]*len(self.model.parameters())
         
-    def step(
-        self, 
-        model_params: list[tuple[np.ndarray, np.ndarray]]
-    ):
-        """Update model params"""
-        for p, g in model_params:
-            if self._weight_decay > 0:
-                g += self._weight_decay * g
-            p -= self._lr * g
+    def step(self):
+        for i, (p_type, p, g) in enumerate(self.model.parameters()):
+            # get regularization grad
+            if p_type == "w": 
+                grad_reg = self.get_grad_reg(p)
+            else:
+                grad_reg = 0.0
 
-    def zero_grad(
-        self, 
-        model_params: list[tuple[np.ndarray, np.ndarray]]
-    ):
-        """Reset grad of model params"""
-        for _, g in model_params:
-            g.fill(0)
+            # add regularization term
+            g += grad_reg
+
+            # calculate new model params
+            if not self.nesterov and self.momentum == 0:
+                p -= self.lr * g
+            else:
+                self.velocity[i] = self.lmbda * self.velocity[i] + (1 - self.lmbda) * g
+                g = g + self.lmbda * self.velocity[i] if self.nesterov else self.velocity[i]
+                p -= self.lr * g
