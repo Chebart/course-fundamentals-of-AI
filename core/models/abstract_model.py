0fbccddff2b094d9e1612dfdc2e1ccabc277cb15
Artyom Chebykin <chebart03@gmail.com>
2025-11-09 22:05:57 +0300

    updated AbstractModel

--- core/models/abstract_model.py
+++ core/models/abstract_model.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-import numpy as np
+from core.data import Tensor
 
 class AbstractModel(ABC):
     def __init__(self):
@@ -9,15 +9,15 @@ class AbstractModel(ABC):
                 "Models must have self.layers attribute!"
             )
 
-    def __call__(self, x: np.ndarray)-> np.ndarray:
+    def __call__(self, x: Tensor)-> Tensor:
         return self.forward(x)
 
     @abstractmethod
-    def forward(self, x: np.ndarray)-> np.ndarray:
+    def forward(self, x: Tensor)-> Tensor:
         """Calculate forward pass"""
         pass
 
-    def parameters(self)-> list[tuple[np.ndarray, np.ndarray]]:
+    def parameters(self)-> list[tuple[str, Tensor, Tensor]]:
         """Return parameters of the model"""
         params = []
         for layer in self.layers:
@@ -25,7 +25,14 @@ class AbstractModel(ABC):
                 params.append((p_type, p, g))
         return params
 
+    def to_device(self, device: str):
+        """Move all model blocks to needed device"""
+        for layer in self.layers:
+            layer.to_device(device)
+
+        return self
+
     @abstractmethod
-    def backward(self, dLdy: np.ndarray)-> None:
+    def backward(self, dLdy: Tensor)-> None:
         """Calculate backward pass"""
         pass
