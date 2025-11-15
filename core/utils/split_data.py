dc9fb84f99a926c74af0a8f43d070096e8d6d18d
Artyom Chebykin <chebart03@gmail.com>
2025-11-15 22:38:30 +0300

    added methods to split data for timeseries

--- core/utils/split_data.py
+++ core/utils/split_data.py
@@ -28,6 +28,43 @@ def train_test_split(
     
     return X_train, X_test, y_train, y_test
 
+def create_sequences(
+    X: Tensor, 
+    y: Tensor, 
+    window_size: int = 1
+)-> tuple[Tensor, Tensor, Tensor, Tensor]:
+    """Create data sequnce"""
+    dtype = X.dtype
+    device = X.device
+    seq_X, seq_y = [], []
+    for i in range(len(X) - window_size):
+        seq_X.append(X[i:i + window_size].to_numpy())
+        seq_y.append(y[i + window_size].to_numpy())
+
+    return Tensor(seq_X, dtype, device), Tensor(seq_y, dtype, device)
+
+def timeseries_train_test_split(
+    X: Tensor, 
+    y: Tensor, 
+    test_size: float = 0.2, 
+    window_size: int = 1
+)-> tuple[Tensor, Tensor, Tensor, Tensor]:
+    """Split timeseries data into train and test subsets"""
+    # Get objects count
+    n_samples = X.shape[0]
+    # Handle test_size
+    n_test = int(n_samples * test_size)
+
+    # Split data
+    X_train, X_test = X[n_test:], X[:n_test]
+    y_train, y_test = y[n_test:], y[:n_test]
+
+    # Get sequences
+    X_train, y_train = create_sequences(X_train, y_train, window_size)
+    X_test, y_test = create_sequences(X_test, y_test, window_size)
+
+    return X_train, X_test, y_train, y_test 
+
 def batch_split(
     X: Tensor, 
     y: Tensor, 
