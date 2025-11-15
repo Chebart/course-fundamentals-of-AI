88b595667b3c75742ff67c6b241417d96b1bb5d8
Artyom Chebykin <chebart03@gmail.com>
2025-11-15 22:39:04 +0300

    fix bug in Tanh

--- core/blocks/tanh.py
+++ core/blocks/tanh.py
@@ -4,9 +4,7 @@ class Tanh(AbstractBlock):
     """tanh(x) = (eˣ - e⁻ˣ) / (eˣ + e⁻ˣ)"""
     
     def forward(self, x):
-        exp_x_plus = x.exp()
-        exp_x_minus = (-x).exp()
-        self.y = (exp_x_plus - exp_x_minus) / (exp_x_plus + exp_x_minus)
+        self.y = x.tanh()
         return self.y
         
     def parameters(self):
