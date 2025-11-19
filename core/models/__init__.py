389ea742eb059b8867e5c1a2d4ecbeb1b284a66f
Artyom Chebykin <chebart03@gmail.com>
2025-11-19 14:03:59 +0300

    added VAE-GAN

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -4,3 +4,4 @@ from .lenet5 import LeNet5
 from .rnn import RNN, RNNClsf, RNNReg
 from .lstm import LSTM, LSTMReg
 from .gru import GRU, GRUReg
+from .vae_gan import VAEEncoder, Generator, Discriminator
---
+++ core/models/vae_gan.py
@@ -0,0 +1,105 @@
+from typing import Literal
+
+from ..blocks import Linear, Sigmoid, Conv2D, ConvTranspose2D, BatchNorm2d, ReLU, LeakyReLU, Reshape
+from .abstract_model import AbstractModel
+from ..data import Tensor
+
+class VAEEncoder(AbstractModel):
+    def __init__(
+        self,
+        in_channels: int,
+        z_dim: int
+    ):
+        self.z_dim = z_dim
+        self.mu = 0
+        self.sigma = 0
+        self.layers = [
+            Conv2D(in_channels, 32, kernel_size = 4, stride = 2, padding = 1),
+            LeakyReLU(0.2),
+            Conv2D(32, 64, kernel_size = 4, stride = 2, padding = 1),
+            LeakyReLU(0.2),
+            Conv2D(64, 128, kernel_size = 4, stride = 2, padding = 1),
+            LeakyReLU(0.2),
+            Reshape(input_shape=(-1, 128, 4, 4), output_shape=(-1, 2048)),
+            Linear(2048, 256),
+            Linear(256, 2 * z_dim)
+        ]
+        super().__init__()
+
+    def forward(self, x):
+        for _, layer in enumerate(self.layers):
+            x = layer(x)
+
+        self.mu = x[:, :self.z_dim]
+        self.sigma = x[:, self.z_dim:]
+        self.std = 0.5 * self.sigma.exp()
+        self.eps = Tensor.random_normal(mean = 0, std = 1, size = self.mu.shape, dtype = self.mu.dtype, device = self.mu.device)
+        return self.mu, self.sigma, self.eps * self.std + self.mu
+    
+    def backward(self, dLdy):
+        for _, layer in enumerate(reversed(self.layers)):
+            dLdy = layer.backward(dLdy)   
+
+class Generator(AbstractModel):
+    def __init__(
+        self,
+        z_dim: int,
+        out_channels: int
+    ):
+        self.layers = [
+            Linear(z_dim, 256),
+            Linear(256, 2048),
+            Reshape(input_shape=(-1, 2048), output_shape=(-1, 128, 4, 4)),
+            ConvTranspose2D(128, 64, kernel_size = 4, stride = 2, padding = 1),
+            BatchNorm2d(64),
+            ReLU(),
+            ConvTranspose2D(64, 32, kernel_size = 4, stride = 2, padding = 1),
+            BatchNorm2d(32),
+            ReLU(),
+            ConvTranspose2D(32, out_channels, kernel_size = 4, stride = 2, padding = 1),
+            Sigmoid()
+        ]
+        super().__init__()
+
+    def forward(self, x):
+        for _, layer in enumerate(self.layers):
+            x = layer(x)
+        return x
+    
+    def backward(self, dLdy):
+        for _, layer in enumerate(reversed(self.layers)):
+            dLdy = layer.backward(dLdy)
+
+        return dLdy
+
+class Discriminator(AbstractModel):
+    def __init__(
+        self,
+        in_channels: int,
+        out_features: int
+    ):
+        self.layers = [
+            Conv2D(in_channels, 32, kernel_size = 4, stride = 2, padding = 1),
+            LeakyReLU(0.2),
+            Conv2D(32, 64, kernel_size = 4, stride = 2, padding = 1),
+            BatchNorm2d(64),
+            LeakyReLU(0.2),
+            Conv2D(64, 128, kernel_size = 4, stride = 2, padding = 1),
+            BatchNorm2d(128),
+            LeakyReLU(0.2),
+            Reshape(input_shape=(-1, 128, 4, 4), output_shape=(-1, 2048)),
+            Linear(2048, out_features),
+            Sigmoid()
+        ]
+        super().__init__()
+
+    def forward(self, x):
+        for idx, layer in enumerate(self.layers):
+            x = layer(x)
+        return x
+    
+    def backward(self, dLdy):
+        for _, layer in enumerate(reversed(self.layers)):
+            dLdy = layer.backward(dLdy)
+
+        return dLdy
