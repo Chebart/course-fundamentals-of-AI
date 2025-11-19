3d8a49f8b7155e7bc29db43810b3d19ee32c040f
Artyom Chebykin <chebart03@gmail.com>
2025-11-19 14:05:58 +0300

    upgrade def plot_curves to work with many y. Adde method to plot mnist images as grid

--- core/utils/draw_plot.py
+++ core/utils/draw_plot.py
@@ -11,7 +11,13 @@ def plot_curves(
 ):
     """Draws multiple curves on the same plot"""
     plt.figure(figsize=(10, 6))
-    plt.plot(x, y, linewidth=2)
+
+    y = np.atleast_2d(y)
+    if y.shape[1] != x.shape[0]:
+        y = y.T
+    for i in range(y.shape[0]):
+        plt.plot(x, y[i], linewidth=2, label=f"Curve {i+1}")
+
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
@@ -37,3 +43,29 @@ def plot_tsne(
     plt.title(title)
     plt.savefig(save_path, dpi=300, bbox_inches="tight")
     plt.close()
+
+def save_mnist_grid(
+    recon: np.ndarray,
+    init: np.ndarray, 
+    save_path: str
+):
+    # get array dims
+    N, H, W = recon.shape
+    # combine preds and targets
+    pairs = np.concatenate([recon, init], axis=2)
+
+    # Compute grid size
+    grid_cols = int(np.ceil(np.sqrt(N)))
+    grid_rows = int(np.ceil(N / grid_cols))
+    total_cells = grid_rows * grid_cols
+    if total_cells > N:
+        pad = total_cells - N
+        pairs = np.concatenate(
+            [pairs, np.zeros((pad, H, 2*W), dtype = pairs.dtype)], 
+            axis=0
+        )
+
+    # reshape grid to quadratic shape
+    grid = pairs.reshape(grid_rows, grid_cols, H, 2 * W)
+    grid = grid.swapaxes(1, 2).reshape(grid_rows * H, grid_cols * 2 * W)
+    np.save(save_path, grid)
